1cd65afdf90bfc3f3957364fd551e660ffbf63f1
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-20 21:28:27 +0700

    Menambahkan getMahasiswaById dan ubahData pada Mahasiswa_model

--- coba-ci/application/models/Mahasiswa_model.php
+++ coba-ci/application/models/Mahasiswa_model.php
@@ -12,4 +12,21 @@ class Mahasiswa_model extends CI_Model
         $this->db->delete('mahasiswa', ['id'=>$id]);
     }
 
+    public function getMahasiswaById($id){
+        return $this->db->get_where('mahasiswa', ['id'=> $id])->row_array();
+    }
+
+    public function ubahDataMahasiswa(){
+        $data = [
+                "nama" => $this->input->post('nama', true),
+                "nrp" => $this->input->post('nrp', true),
+                "email" => $this->input->post('email', true),
+                "email" => $this->input->post('jurusan', true),
+        ];
+        $this->db->where('id', $this->input->post('id'));
+        
+        $this->db->update('mahasiswa', $data);
+        
+    }
+
 }
